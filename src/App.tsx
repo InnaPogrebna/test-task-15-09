986a33a94905b8f49d10d0b9818933ff7646e4ac
Inna <inna_temis@ukr.net>
2022-09-16 19:06:46 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import {
+  Route, Routes, Link,
+} from 'react-router-dom';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import { Page1 } from './pages/Page1';
+import { Page2 } from './pages/Page2';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="page">
+      <nav className="navbar">
+        {/* <Link className="navbar-item" to="/home">
+          Back
+        </Link> */}
+        <Link className="navbar-item" to="/page1">
+          Page1
+        </Link>
+        <Link className="navbar-item" to="/page2">
+          Page2
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="page2" element={<Page2 />} />
+        <Route path="page1" element={<Page1 />} />
+      </Routes>
     </div>
   );
 };
